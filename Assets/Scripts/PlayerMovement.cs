c75dcabf636ebb149b2507bf59b9f806436fac85
Nabeel Ahmed <nabeelrajpoot314@gmail.com>
2020-09-07 11:53:23 +0500

    first

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D Player;
+    float horizotal , vertical;
+    private bool isFacingright = true;
+    SpriteRenderer image;
+
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GetComponent<Rigidbody2D>();
+        image = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizotal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(horizotal, vertical) * speed;
+        Player.velocity = movement;
+        
+        if ( horizotal < 0 )
+        {
+            image.flipX = true;
+            isFacingright = false;
+            
+        }
+        else if ( horizotal > 0)
+        {
+            image.flipX = false;
+            isFacingright = true;
+            
+        }
+
+
+    }
+    
+    public void FlipFace()
+    {
+        isFacingright = !isFacingright;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
+    }
+
+
+}
---
+++ Assets/Scripts/scroll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll : MonoBehaviour
+{
+    public float bgspeed;
+    public Renderer bgrend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bgrend.material.mainTextureOffset += new Vector2(bgspeed * Time.deltaTime, 0f);
+    }
+}
